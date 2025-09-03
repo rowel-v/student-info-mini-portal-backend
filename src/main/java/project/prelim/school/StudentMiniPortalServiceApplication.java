19bc49c51dedcbd9fa4738b6ef550190b8b1e284
rowel <razonabler23@gmail.com>
2025-09-03 20:39:20 +0800

    initialize project

---
+++ src/main/java/project/prelim/school/StudentMiniPortalServiceApplication.java
@@ -0,0 +1,13 @@
+package project.prelim.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMiniPortalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMiniPortalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/prelim/school/StudentMiniPortalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package project.prelim.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMiniPortalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
